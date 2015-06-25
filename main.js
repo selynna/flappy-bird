71f3d09153451646accdae8a808e646f032d1966
Selynna Sun <ssun098@gmail.com>
2015-06-25 15:48:30 -0700

    struggles

--- main.js
+++ main.js
@@ -15,10 +15,14 @@ mainState.preload = function() {
 
 };
 
-mainState.create = function() {
+mainState.create = function() { 
 
 	//set physics system
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.add.tileSprite(0,0,400,490, 'background');
+	game.add.tileSprite(0, 420, 400, 490, 'ground');
+	game.add.tileSprite(0, 0,400, 200,'title');
+	game.add.tileSprite(130, 250, 170, 350, 'start-button');
 	this.bird = this.game.add.sprite(100, 245, 'bird');
 	//add gravity
 	game.physics.arcade.enable(this.bird);
@@ -47,8 +51,8 @@ mainState.update = function() {
 		this.restartGame();
 
 	}
-	game.physics.arcade.overlap(this.bird, this.pipes, 
-		this.hitPipe, null, this);
+	
+	game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
 	if (this.bird.angle < 20) {
 		this.bird.angle += 1;
 	}
